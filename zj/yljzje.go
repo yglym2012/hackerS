4e64a9d5e903eda6ad2c8771b7742557ae7d6ac4
yglym2012 <448103472@qq.com>
2017-03-14 16:19:09 +0800

    3

--- zj/yljzje.go
+++ zj/yljzje.go
@@ -71,7 +71,7 @@ func (t *SimpleChaincode) add(stub shim.ChaincodeStubInterface, args []string) (
 		}
 	} else {
 		var l []string
-		l = string(ListIDvalTemp)
+		l = []string(ListIDvalTemp)
 		l = append(l, NListIDval)
 		byteContent := "\x00" + strings.Join(l, "\x02\x00")
 		err = stub.PutState(NListID, []byte(byteContent))
